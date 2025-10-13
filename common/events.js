2215ca67c898024a128bcc69c1e9bdb156104610
arthurrguedes <arthurguedes_29@hotmail.com>
2025-10-13 19:11:01 -0300

    first commit

---
+++ common/events.js
@@ -0,0 +1,8 @@
+// o "module.exports = {" impedia de subir o serviço pois não é o padrão (ES Modules - export) utilizado no projeto
+
+export const ROUTING_KEYS = {
+  ROUTING_KEYS: {
+    USER_CREATED: 'user.created',
+    ORDER_CREATED: 'order.created'
+  }
+};
